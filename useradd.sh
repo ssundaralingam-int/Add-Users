1e8577df7f2b5aa9859276c8baac7850ada005b7
ssundaralingam-int <37655145+ssundaralingam-int@users.noreply.github.com>
2018-03-22 17:10:30 +0530

    Update useradd.sh

--- useradd.sh
+++ useradd.sh
@@ -1,3 +1,3 @@
-bash /var/www/html/postfixadmin-3.1/scripts/postfixadmin-cli mailbox add emailer3@testserver2.com | ssh root@172.31.1.236 "cat>> pwdfile.txt"
+bash /var/www/html/postfixadmin-3.1/scripts/postfixadmin-cli mailbox add emailer5@testserver2.com | ssh root@172.31.1.236 "cat>> pwdfile.txt"
 #bash /var/www/html/postfixadmin-3.1/scripts/postfixadmin-cli mailbox add abuse12@testserver2.com >> pwd.txt
 #bash /var/www/html/postfixadmin-3.1/scripts/postfixadmin-cli mailbox add bounce12@testserver2.com >> pwd.txt
